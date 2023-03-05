cb9a23750a7730e25569c722abd4180bf4f1d3bc
buianhtai1205 <buianhtai2017tq@gmail.com>
2023-03-05 13:47:34 +0700

    update(share title): --construct

--- app/Http/Controllers/SuperAdmin/SuperAdminController.php
+++ app/Http/Controllers/SuperAdmin/SuperAdminController.php
@@ -6,9 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Route;
 
 class SuperAdminController extends Controller
 {
+
+    public function __construct()
+    {
+        $routeName = Route::currentRouteName();
+        $arr = explode('.', $routeName);
+        $arr = array_map('ucfirst', $arr);
+        $title = implode(' / ', $arr);
+        \Illuminate\Support\Facades\View::share('title', $title);
+    }
+
     public function  index(): Factory|View|Application
     {
         return view('super_admin.index');
